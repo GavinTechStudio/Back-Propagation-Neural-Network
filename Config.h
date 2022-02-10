43611c7aee6c3449ded242e0a1fd785c85054cf0
Gavin Sun <gavinsun0921@foxmail.com>
2022-02-10 19:23:00 +0800

    add Config.h

---
+++ Config.h
@@ -0,0 +1,16 @@
+//
+// Created by gavin on 2022/2/10.
+//
+
+#ifndef BPNN_CONFIG_H
+#define BPNN_CONFIG_H
+
+
+namespace Config {
+    const int INNODE = 2;
+    const int HIDENODE = 4;
+    const int OUTNODE = 1;
+};
+
+
+#endif //BPNN_CONFIG_H
