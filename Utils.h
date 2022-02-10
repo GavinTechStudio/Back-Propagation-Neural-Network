08c64b5ed00413753882c6c1661222c2943f5511
Gavin Sun <gavinsun0921@foxmail.com>
2022-02-10 19:22:17 +0800

    add Utils

---
+++ Utils.h
@@ -0,0 +1,28 @@
+//
+// Created by gavin on 2022/2/10.
+//
+
+#ifndef BPNN_UTILS_H
+#define BPNN_UTILS_H
+
+
+#include <cmath>
+#include <vector>
+#include <string>
+#include "Sample.h"
+
+using std::vector;
+using std::string;
+
+namespace Utils {
+    static double sigmoid(double x) {
+        return 1.0 / (1.0 + std::exp(-x));
+    }
+
+    vector<double> getFileData(const string& filename);
+    vector<Sample> getTrainData(const string& filename);
+    vector<Sample> getTestData(const string& filename);
+};
+
+
+#endif //BPNN_UTILS_H
