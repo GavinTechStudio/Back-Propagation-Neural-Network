a5918e01415c3947071fff0eb045c4d4db104bc4
Gavin Sun <gavinsun0921@foxmail.com>
2022-02-10 19:23:18 +0800

    add Sample.h

---
+++ Sample.h
@@ -0,0 +1,17 @@
+//
+// Created by gavin on 2022/2/10.
+//
+
+#ifndef BPNN_SAMPLE_H
+#define BPNN_SAMPLE_H
+
+#include <vector>
+
+using std::vector;
+
+struct Sample {
+    vector<double> in, out;
+};
+
+
+#endif //BPNN_SAMPLE_H
