efafd8eaaa7e743930f4853b5edb70462508b40d
Gavin Sun <gavinsun0921@foxmail.com>
2022-02-10 19:42:53 +0800

    update Net.cpp => finish constructor

--- Net.cpp
+++ Net.cpp
@@ -17,7 +17,7 @@ Net::Net() {
         // 输入层的神经元节点不需要偏置值
         // inputLayer[i]->bias = distribution(rd);
         for (size_t j = 0; j < Config::HIDENODE; ++j) {
-            // 初始化输入层第 i 个神经元到隐藏层第 j 个神经元的权重值
+            // 初始化输入层第 i 个神经元到隐藏层第 j 个神经元的权重
             inputLayer[i]->weight[j] = distribution(rd);
             // 初始化输入层第 i 个神经元到隐藏层第 j 个神经元的权重修正值
             inputLayer[i]->weight_delta[j] = 0.f;
@@ -26,7 +26,26 @@ Net::Net() {
 
     // 初始化隐藏层
     for (size_t j = 0; j < Config::HIDENODE; ++j) {
+        hideLayer[j] = new Node(Config::OUTNODE);
+        // 初始化隐藏层神经元系节点偏置值
+        hideLayer[j]->bias = distribution(rd);
+        // 初始化隐藏层神经元系节点偏置值修正值
+        hideLayer[j]->bias_delta = 0.f;
+        for (size_t k = 0; k < Config::OUTNODE; ++k) {
+            // 初始化隐藏层第 j 个神经元到输出层第 k 个神经元的权重
+            hideLayer[j]->weight[k] = distribution(rd);
+            // 初始化隐藏层第 j 个神经元到输出层第 k 个神经元的权重修正值
+            hideLayer[j]->weight_delta[k] = 0.f;
+        }
+    }
 
+    // 初始化输出层
+    for (size_t k = 0; k < Config::OUTNODE; ++k) {
+        outputLayer[k] = new Node(0);
+        // 初始化输出层神经元系节点偏置值
+        outputLayer[k]->bias = distribution(rd);
+        // 初始化输出层神经元系节点偏置值偏置值
+        outputLayer[k]->bias_delta = 0.f;
     }
 }
 
