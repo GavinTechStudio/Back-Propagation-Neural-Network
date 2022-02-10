8992fad0ed9067df20ead49d2962016b60aca84c
Gavin Sun <gavinsun0921@foxmail.com>
2022-02-10 19:46:29 +0800

    update Config.h => add 'lr', 'threshold' and 'mosttimes'

--- Config.h
+++ Config.h
@@ -5,11 +5,16 @@
 #ifndef BPNN_CONFIG_H
 #define BPNN_CONFIG_H
 
+using std::size_t;
 
 namespace Config {
-    const int INNODE = 2;
-    const int HIDENODE = 4;
-    const int OUTNODE = 1;
+    const size_t INNODE = 2;
+    const size_t HIDENODE = 4;
+    const size_t OUTNODE = 1;
+
+    const double lr = 0.8;
+    const double threshold = 1e-4;
+    const size_t mosttimes = 1e6;
 };
 
 
